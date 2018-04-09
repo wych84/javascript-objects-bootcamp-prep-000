1c0c8099c961c9381bc62b374560fa80a9c1a6ac
Joyce Chun <wooyangee@gmail.com>
2018-04-09 06:58:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var playlist = { mariahCarey:"Hero", psy:"Kangnam style", shakira:"pies descalzos" }
+=======
+var playlist = { mariahCarey:"Hero", psy:"Kangnam style" }
+>>>>>>> 5107d997517da963b472a0aaaca24f594da9b179
 
 
 
@@ -6,6 +10,7 @@ function updatePlaylist(obj, key, value) {
   obj[key] = value
   return obj
 }
+<<<<<<< HEAD
 
 
 function removeFromPlaylist(obj, key) {
@@ -13,3 +18,9 @@ function removeFromPlaylist(obj, key) {
   return obj
 }
 
+=======
+const obj = { psy:"kangnam style"}
+updatePlaylist(obj, 'Alison', 'My bloody Valentine')
+
+
+>>>>>>> 5107d997517da963b472a0aaaca24f594da9b179
